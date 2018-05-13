4538c696e90261624b5bc2dad650b9257efebbf3
ElenaKusevska <elena.kusevska@gmail.com>
2018-05-13 19:50:14 -0400

    small changes to gauss_output

--- gauss_output.sh
+++ gauss_output.sh
@@ -11,7 +11,17 @@ elif [ $# -ne 0 ]; then
       if [ -d "$(pwd)/$i" ]; then
          cd $i
          if [ -f "$(pwd)/$i.out" ]; then
+            # reset thermochemistry variables:
+            energy=' '
+            zero_point=' '
+            enthalpy=' '
+            gibbs=' '
+
+            # start processing the putput file
+            echo '--------------------------------'
             echo $i
+            echo '--------------------------------'
+            echo ' '
             echo ' ' | cat >> output
             echo '--------------------------------' | cat >> output
             echo $i: | cat >> output
@@ -61,17 +71,19 @@ elif [ $# -ne 0 ]; then
             grep 'Thermal correction' $i.out >> output
             grep 'Sum of electronic' $i.out >> output
 
-            zero_point=$(grep 'Sum of electronic and zero-point Energies=' *.out | tail -1 | cut -c55-65)
+            zero_point=$(grep 'Sum of electronic and zero-point Energies=' *.out | tail -1 | cut -c54-65)
 
-            enthalpy=$(grep 'Sum of electronic and thermal Enthalpies=' *out | tail -1 | cut -c55-65)
+            enthalpy=$(grep 'Sum of electronic and thermal Enthalpies=' *out | tail -1 | cut -c54-65)
 
-            gibbs=$(grep 'Sum of electronic and thermal Free Energies=' *out | tail -1 | cut -c55-65)
+            gibbs=$(grep 'Sum of electronic and thermal Free Energies=' *out | tail -1 | cut -c54-65)
 
             echo ' ' | cat >> output
             cd ..
-            
+
+            echo ' '
             echo 'thermochemistry (E, E0, H, G):'
             echo $energy $zero_point $enthalpy $gibbs
+            echo ' '
          else
             echo the file $(pwd)/$i.out does not exist
          fi
