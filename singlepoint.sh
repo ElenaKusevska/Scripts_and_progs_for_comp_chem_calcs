f0ca18ebd22243584ea50cf30dd75eb717b7b787
ElenaKusevska <elena.kusevska@gmail.com>
2018-05-18 14:19:33 -0400

    singlepoint.sh

---
+++ singlepoint.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+np=8 # number of processors
+m=8 # memory
+
+for i in 4
+do
+  echo $i
+  cd $i
+
+  #-----------------------------------------------
+  # Prepare the Gaussian input file:
+  #-----------------------------------------------
+
+  echo %NProc=$np | cat >> temp
+  echo %Mem=$m'GB' | cat >> temp
+  echo %chk=$i.chk | cat >> temp
+  echo '#p m062x/6-311+g(d,p) scrf=(smd,solvent=chloroform) Geom=Checkpoint pop=nbo' | cat >> temp
+  echo ' ' | cat >> temp
+  echo $i | cat >> temp
+  echo ' ' | cat >> temp
+
+  sed -n 8p $i.g09 >> temp # just the line with charge and
+                               # multiplicity
+
+  echo ' ' | cat >> temp
+  echo ' ' | cat >> temp
+
+  rm $i.g09
+  mv temp $i.g09
+  rm -f $i.out $i.slurm $i.fchk $i.xyz output
+
+  #-------------------------------------------
+  # Prepare the slurm script:
+  #-------------------------------------------
+
+  echo '#!/usr/bin/env bash' | cat >> $i.slurm
+  echo '#SBATCH --time=96:00:00' | cat >> $i.slurm
+  echo '#SBATCH --nodes=1' | cat >> $i.slurm
+  echo '#SBATCH --ntasks=1' | cat >> $i.slurm
+  echo '#SBATCH --cpus-per-task='$np | cat >> $i.slurm
+  echo '#SBATCH --mem='$m'gb' | cat >> $i.slurm
+  echo '#SBATCH --job-name='$i | cat >> $i.slurm
+  echo '#SBATCH --output='$i.out | cat >> $i.slurm
+  echo '#SBATCH --partition=smp' | cat >> $i.slurm
+  echo ' ' | cat >> $i.slurm
+  echo module purge | cat >> $i.slurm
+  echo module load gaussian/D.01 | cat >> $i.slurm
+  echo ' ' | cat >> $i.slurm
+  echo cp $i.g09 '$SLURM_SCRATCH' | cat >> $i.slurm
+  echo cp $i.chk '$SLURM_SCRATCH' | cat >> $i.slurm
+  echo cd '$SLURM_SCRATCH' | cat >> $i.slurm
+  echo ' ' | cat >> $i.slurm
+  echo ulimit -s unlimited | cat >> $i.slurm
+  echo export LC_COLLATE=C | cat >> $i.slurm
+  echo ' ' | cat >> $i.slurm
+  echo 'g09 < $SLURM_JOB_NAME.g09' | cat >> $i.slurm
+  echo cp $i.chk '$SLURM_SUBMIT_DIR' | cat >> $i.slurm
+  echo ' ' | cat >> $i.slurm
+  echo formchk $i.chk $i.fchk | cat >> $i.slurm
+  echo cp $i.fchk '$SLURM_SUBMIT_DIR' | cat >> $i.slurm
+  echo ' ' | cat >> $i.slurm
+  echo ' ' | cat >> $i.slurm
+
+  cd ..
+  sleep 3
+done
+
