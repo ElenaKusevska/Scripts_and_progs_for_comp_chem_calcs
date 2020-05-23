c041f746c7e4db0ae447dc16cb5d49e1b2e56826
ElenaKusevska <elena.kusevska@gmail.com>
2020-05-23 16:24:45 +0200

    test parallel

---
+++ 4_set_up_cluster/test_parallel/runs_parallel_across_nodes/sh2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo '2 '$(date +"%T %N")' start' >> output
+sleep 10
+echo '2 '$(date +"%T %N")' end' >> output
---
+++ 4_set_up_cluster/test_parallel/runs_parallel_within_node/sh1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo '1 '$(date +"%T %N")' start' >> output
+sleep 10
+echo '1 '$(date +"%T %N")' end' >> output
